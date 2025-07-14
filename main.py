6d37824c592004e9758fbd435fd24f52bcd0c5e3
Rastilan <christopherbhall@live.com>
2025-07-14 17:09:04 -0400

    B: Refactored code to better call from main.py and cleaned up overall functionality

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from stats import count_words;
-
+from stats import count_words, count_chars
+file_contents = ""
 def main():
     with open("books/frankenstein.txt") as f:
-        file_contents = f.read();
+        file_contents = f.read()
+        word_count = count_words(file_contents)
+        char_count = count_chars(file_contents)
+        print(f'{word_count} words found in the document')
+        print(char_count)
 
-    print(file_contents);
 
-#main();
+main();
--- stats.py
+++ stats.py
@@ -1,25 +1,20 @@
-def count_words():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read();
-    words = file_contents.split();
-    num_words = len(words);
-    print(f'{num_words} words found in the document')
 
-def count_chars():
-    char_count = {}
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read();
-        words = file_contents.split();
-        for word in words:
-            for letter in word:
-                letter = letter.lower();
-                if letter in char_count:
-                    char_count[letter] += 1;
+def count_words(contents):
+    words = contents.split()
+    num_words = len(words)
+    return num_words
+
+def count_chars(contents):
+    chars = {}
+    for word in contents:
+        for letter in word:
+            letter = letter.lower()
+            if letter in chars:
+                chars[letter] += 1
+            
+            else:
+                chars[letter] = 1
                 
-                else:
-                    char_count[letter] = 1;
-                
-    print(char_count);
+    return chars
 
 
-count_chars();
